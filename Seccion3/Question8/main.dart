6a2a0b76f27ad962d6bb59298f21129ca01da7ac
Hamza <alhnawyh38@gmial.com>
2024-12-25 01:32:37 +0200

    Finish task 4

--- Seccion3/Question8/main.dart
+++ Seccion3/Question8/main.dart
@@ -1,14 +1,3 @@
 import 'dart:io';
 
-void main() {
-  print('Enter your tempature:');
-  int temperature = int.parse(stdin.readLineSync()!);
-
-  if (temperature > 30) {
-    print('it is hot');
-  } else if (temperature > 15 || temperature < 30) {
-    print('it is warm');
-  } else {
-    print('it is cold');
-  }
-}
+void main() {}
